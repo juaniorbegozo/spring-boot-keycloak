9d83f9127459e60c5bf4bc461cc8abc999bd5c05
Juan Orbegozo <you@example.com>
2019-07-05 16:44:33 -0300

    first commit

---
+++ src/main/java/com/example/LoginController.java
@@ -0,0 +1,51 @@
+package com.example;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class LoginController {
+
+
+  
+
+  @GetMapping(value = "/all")
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  public void search() {
+  }
+
+  /*@GetMapping(value = "/info")
+  @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_CLIENT')")
+  public UserResponseDTO whoami(HttpServletRequest req) {
+    return modelMapper.map(userService.whoami(req), UserResponseDTO.class);
+  }*/
+  
+  /*
+  @DeleteMapping(value = "/{username}")
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  @ApiOperation(value = "${UserController.delete}")
+  @ApiResponses(value = {//
+      @ApiResponse(code = 400, message = "Something went wrong"), //
+      @ApiResponse(code = 403, message = "Access denied"), //
+      @ApiResponse(code = 404, message = "The user doesn't exist"), //
+      @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+  public String delete(@ApiParam("Username") @PathVariable String username) {
+    userService.delete(username);
+    return username;
+  }
+  
+   @PostMapping("/signup")
+  @ApiOperation(value = "${UserController.signup}")
+  @ApiResponses(value = {//
+      @ApiResponse(code = 400, message = "Something went wrong"), //
+      @ApiResponse(code = 403, message = "Access denied"), //
+      @ApiResponse(code = 422, message = "Username is already in use"), //
+      @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+  public String signup(@ApiParam("Signup User") @RequestBody UserDataDTO user) {
+    return userService.signup(modelMapper.map(user, User.class));
+  }
+	*/
+}
---
+++ src/main/java/com/example/SpringBootKeycloakApplication.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKeycloakApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKeycloakApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
